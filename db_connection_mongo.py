aafa7b50915b8ad56395457ff462da5fafe650d7
fidelisprasetyo <fidelisprasetyo@gmail.com>
2024-10-12 19:58:10 -0700

    some progress

--- db_connection_mongo.py
+++ db_connection_mongo.py
@@ -19,79 +19,38 @@ def connectDataBase():
     except:
         print("Database not connected successfully")
 
-def createUser(col, id, name, email):
+def createDocument(col, id, text, title, date, category):
 
-    # Value to be inserted
-    user = {"_id": id,
-            "name": name,
-            "email": email,
-            }
+    doc = {"_id": id,
+            "text": text,
+            "title": title,
+            "date":  date,
+            "category": category}
+    
+    col.insert_one(doc)
 
-    # Insert the document
-    col.insert_one(user)
+def updateDocument(col, id, text, title, date, category):
 
-def updateUser(col, id, name, email):
+    doc = {"$set": {
+            "text": text,
+            "title": title,
+            "date": date,
+            "category": category}}
+    
+    col.update_one({"_id": id}, doc)
 
-    # User fields to be updated
-    user = {"$set": {"name": name, "email": email} }
+def deleteDocument(col, id):
 
-    # Updating the user
-    col.update_one({"_id": id}, user)
+    col.detele_one({"_id": id})
 
-def deleteUser(col, id):
+def getIndex(col):
+    
+    pipeline = [{"$split": {}}]
 
-    # Delete the document from the database
-    col.delete_one({"_id": id})
 
-def getUser(col, id):
-
-    user = col.find_one({"_id":id})
-
-    if user:
-        return str(user['_id']) + " | " + user['name'] + " | " + user['email']
-    else:
-        return []
-
-def createComment(col, id_user, dateTime, comment):
-
-    # Comments to be included
-    comments = {"$push": {"comments": {
-                                       "datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S"),
-                                       "comment": comment
-                                       } }}
-
-    # Updating the user document
-    col.update_one({"_id": id_user}, comments)
-
-def updateComment(col, id_user, dateTime, new_comment):
-
-    # User fields to be updated
-    comment = {"$set": {"comments.$.comment": new_comment} }
-
-    # Updating the user
-    col.update_one({"_id": id_user, "comments.datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")}, comment)
-
-def deleteComment(col, id_user, dateTime):
-
-    # Comments to be delete
-    comments = {"$pull": {"comments": {"datetime": datetime.datetime.strptime(dateTime, "%m/%d/%Y %H:%M:%S")} }}
-
-    # Updating the user document
-    col.update_one({"_id": id_user}, comments)
-
-def getChat(col):
-
-    # creating a document for each message
-    pipeline = [
-                 {"$unwind": { "path": "$comments" }},
-                 {"$sort": {"comments.datetime": 1}}
-               ]
-
-    comments = col.aggregate(pipeline)
-
-    chat = ""
-
-    for com in comments:
-        chat += com['name'] + " | " + com['comments']['comment'] + " | " + str(com['comments']['datetime']) + "\n"
-
-    return chat
+    for doc in col.find():
+        termcount = {}
+        for term in doc["text"].split():
+            lterm = term.lower()
+            termcount[lterm] = termcount.get(lterm, 0) + 1
+        print(termcount)
